0acc31e4eb11cf58aca95f827efa40a8f2440c42
amir <afwan70@gmail.com>
2018-12-14 08:51:38 +0800

    Persisting data using codable protocol and plist

--- TodoWork/Controllers/TodoListViewController.swift
+++ TodoWork/Controllers/TodoListViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class TodoListViewController: UITableViewController  {
     
-    var itemArray = ["jogging","studying","cooking"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(dataFilePath)
+        
+        loadItems()
+        
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "TodoListArray") as?  [Item] {
+//            itemArray = items
+//        }
+        
     }
     
     //MARK - Tableview Datasource Methods
@@ -31,7 +37,11 @@ class TodoListViewController: UITableViewController  {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -39,13 +49,10 @@ class TodoListViewController: UITableViewController  {
     //MARK - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
+       
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark 
-        }
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -61,9 +68,12 @@ class TodoListViewController: UITableViewController  {
         
         let action = UIAlertAction(title: "Add Work", style: .default) { (action) in
             //what will hapen once user click the add work button on UIAlert
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+           
         }
         
         alert.addTextField { (alertTextField) in
@@ -75,5 +85,30 @@ class TodoListViewController: UITableViewController  {
         present(alert, animated: true, completion: nil)
     }
     
-}
+    //MARK - Model manipulation methods
 
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to : self.dataFilePath!)
+        }
+        catch {
+            print("Error encoding, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding, \(error)")
+            }
+        }
+    }
+    
+}
---
+++ TodoWork/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TodoWork
+//
+//  Created by AMIR AFWAN on 12/13/18.
+//  Copyright © 2018 AMIR AFWAN. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title : String = ""
+    var done : Bool = false
+}
